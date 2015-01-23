30664b14675deb0ea234d2f6b5bb701410cbaea7
bishoy.shaheer@gmail.com <bishoy.shaheer@gmail.com>
2015-01-23 15:53:51 +0200

    Merge origin/master
    Conflicts:
    	client/nbproject/project.properties

--- client/src/controller/ClientAction.java
+++ client/src/controller/ClientAction.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Server;
+package controller;
 
 import model.Contact;
 
@@ -11,17 +11,17 @@ import model.Contact;
  *
  * @author sarah
  */
-class ClientAction {
+public class ClientAction {
     final static int addContactServiceNo=1;
     final static int removeContactServiceNo=2;
     final static int acceptFriendServiceNo=3;
     final static int changeStateServiceNo=4;
     final static int writeStateServiceNo=5;
     //must implemented by radwa to send servicde no
-    int getServiceNo(){
+    public int getServiceNo(){
         return 1;
     }
-  Contact getContact(){
+  public Contact getContact(){
         Contact c=new Contact("sarahAabed@yahoo","Sarah", null, null, null);
         return c;
     }
--- client/src/rmi/server/ServerListner.java
+++ client/src/rmi/server/ServerListner.java
@@ -3,8 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Server;
+package rmi.server;
 
+import controller.ClientAction;
+import controller.IserverController;
+import controller.ServerController;
 import java.rmi.Remote;
 
 /**
